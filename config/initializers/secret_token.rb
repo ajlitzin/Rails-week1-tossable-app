6c9b6db3a48f91630e74e16cdd2374dd4815f288
Andy Litzinger <andy.litzinger@gmail.com>
2012-01-14 16:46:10 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BooksIRead::Application.config.secret_token = '70232c310b290c78687535918a675f12ee24ebc9ccd54f406a9739cdf2a7771ee50d010edeed0833df46aa1cd292edbb14f135210af310f828ddf0719a0811ad'
